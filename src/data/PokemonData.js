9ef2d25c6a719da39eead2c5c367547b482f1626
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-16 15:29:46 +0100

    Pokedex game with data only

---
+++ src/data/PokemonData.js
@@ -0,0 +1,320 @@
+const data = {
+
+    pokemon: [
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+            name: 'Bulbasaur',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+            name: 'Ivysaur',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+            name: 'Venusaur',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+            name: 'Charmander',
+            attribute: ['Fire']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+            name: 'Charmeleon',
+            attribute: ['Fire']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+            name: 'Charizard',
+            attribute: ['Fire', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+            name: 'Squirtle',
+            attribute: ['Water']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+            name: 'Wartortle',
+            attribute: ['Water']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png',
+            name: 'Blastoise',
+            attribute: ['Water']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png',
+            name: 'Caterpie',
+            attribute: ['Bug']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png',
+            name: 'Metapod',
+            attribute: ['Bug']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
+            name: 'Butterfree',
+            attribute: ['Bug', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png',
+            name: 'Weedle',
+            attribute: ['Bug', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/014.png',
+            name: 'Kakuna',
+            attribute: ['Bug', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png',
+            name: 'Beedrill',
+            attribute: ['Bug', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png',
+            name: 'Pidgey',
+            attribute: ['Normal', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png',
+            name: 'Pidgeotto',
+            attribute: ['Normal', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png',
+            name: 'Pidgeot',
+            attribute: ['Normal', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/019.png',
+            name: 'Rattata',
+            attribute: ['Normal']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/020.png',
+            name: 'Raticate',
+            attribute: ['Normal']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png',
+            name: 'Spearow',
+            attribute: ['Normal', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://www.pokemon.com/uk/pokedex/fearow',
+            name: 'Fearow',
+            attribute: ['Normal', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/023.png',
+            name: 'Ekans',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png',
+            name: 'Arbok',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+            name: 'Pikachu',
+            attribute: ['Electric']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png',
+            name: 'Raichu',
+            attribute: ['Electric']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/027.png',
+            name: 'Sandshrew',
+            attribute: ['Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png',
+            name: 'Sandslash',
+            attribute: ['Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/029.png',
+            name: 'Nidoran',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png',
+            name: 'Nidorina',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/031.png',
+            name: 'Nidoqueen',
+            attribute: ['Poison', 'Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/032.png',
+            name: 'Nidoran',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/033.png',
+            name: 'Nidorino',
+            attribute: ['Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/034.png',
+            name: 'Nidoking',
+            attribute: ['Poison', 'Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png',
+            name: 'Clefairy',
+            attribute: ['Fairy']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/036.png',
+            name: 'Clefable',
+            attribute: ['Fairy']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png',
+            name: 'Vulpix',
+            attribute: ['Fire']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png',
+            name: 'Ninetales',
+            attribute: ['Fire']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png',
+            name: 'Jigglypuff',
+            attribute: ['Normal', 'Fairy']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png',
+            name: 'Wigglytuff',
+            attribute: ['Normal', 'Fairy']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/041.png',
+            name: 'Zubat',
+            attribute: ['Poison', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/042.png',
+            name: 'Golbat',
+            attribute: ['Poison', 'Flying']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/043.png',
+            name: 'Oddish',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/044.png',
+            name: 'Gloom',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/045.png',
+            name: 'Vileplume',
+            attribute: ['Grass', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/046.png',
+            name: 'Paras',
+            attribute: ['Bug', 'Grass']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/047.png',
+            name: 'Parasect',
+            attribute: ['Bug', 'Grass']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/048.png',
+            name: 'Venonat',
+            attribute: ['Bug', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/049.png',
+            name: 'Venomoth',
+            attribute: ['Bug', 'Poison']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png',
+            name: 'Diglett',
+            attribute: ['Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/051.png',
+            name: 'Dugtrio',
+            attribute: ['Ground']
+        },
+
+        {
+            imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png',
+            name: 'Meowth',
+            attribute: ['Normal']
+        }
+        
+    ]
+
+}
+
+export default data
