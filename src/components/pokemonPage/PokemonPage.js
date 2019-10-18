63d1e49b411d205843afa4d05daf3fb489298930
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-18 09:43:48 +0100

    Pokedex game with search trial 1

--- src/components/pokemonPage/PokemonPage.js
+++ src/components/pokemonPage/PokemonPage.js
@@ -1,22 +1,39 @@
 import React, {Fragment, useState} from 'react';
+import Lodash from 'lodash';
 import PokemonSearch from './pokemonSearch/PokemonSearch';
 import PokemonBody from './pokemonBody/PokemonBody';
 
 const PokemonPage = ({info}) => {
 
     const [userText, setUserText] = useState('')
+    const [pokemonData, setPokemonData] = useState(info)
+    // const [searchValue, setSearchValue] = useState(null)
 
-    const updateUserText = (event) => {
+    
+
+    const updateSearchValue = (event) => {
         event.preventDefault();
     
         setUserText(event.target.value);
         console.log('this is the', userText)
+       
     }
+
+  const upodatePokemonData = () =>{
+    _.filter(pokemonData, function(pokemon) { return pokemon.pokemonName.includes(userText); });
+  }
+
+useEffect(()=>{
+    searchPokemon()
+    if(userText) {
+        debounce( upodatePokemonData, 250, { 'maxWait': 750 } )
+    }
+},[userText])
 
     return (
         <>
-            <PokemonSearch value={userText} onChange={updateUserText} />
-            <PokemonBody info={info} />
+            <PokemonSearch value={userText} onChange={(e)=>updateSearchValue(e)} />
+            <PokemonBody info={pokemonData} />
         </>
     )
 }
