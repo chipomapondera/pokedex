5042bdd9c0b8e39ff669dc4311ae9ee2c447da2f
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-18 10:28:35 +0100

    Pokedex game search with updatePokemonData funtion working, useEffect not worlking

--- src/components/pokemonPage/PokemonPage.js
+++ src/components/pokemonPage/PokemonPage.js
@@ -1,5 +1,5 @@
-import React, {Fragment, useState} from 'react';
-import Lodash from 'lodash';
+import React, {Fragment, useState, useEffect} from 'react';
+import {filter, debounce} from 'lodash';
 import PokemonSearch from './pokemonSearch/PokemonSearch';
 import PokemonBody from './pokemonBody/PokemonBody';
 
@@ -11,29 +11,37 @@ const PokemonPage = ({info}) => {
 
     
 
-    const updateSearchValue = (event) => {
-        event.preventDefault();
+    // const updateSearchValue = (event) => {
+    //     event.preventDefault();
     
-        setUserText(event.target.value);
-        console.log('this is the', userText)
+    //     setUserText(event.target.value);
+    //     console.log('this is the', userText)
        
-    }
+    // }
+    // console.log(filter(pokemonData, function(pokemon) { 
+    //     const normalPokemonName = pokemon.pokemonName.toLowerCase()
+    //     return normalPokemonName.includes('bul') })
+    //     )
 
+    // console.log('asdasd', userText)
   const upodatePokemonData = () =>{
-    _.filter(pokemonData, function(pokemon) { return pokemon.pokemonName.includes(userText); });
+      console.log('232234')
+    const filterData = filter(info, function(pokemon) { 
+        const normalPokemonName = pokemon.pokemonName.toLowerCase()
+        return normalPokemonName.includes(userText);
+    });
+    setPokemonData(filterData)
   }
 
 useEffect(()=>{
-    searchPokemon()
-    if(userText) {
         debounce( upodatePokemonData, 250, { 'maxWait': 750 } )
-    }
 },[userText])
 
+const finalData = userText ? pokemonData : info
     return (
         <>
-            <PokemonSearch value={userText} onChange={(e)=>updateSearchValue(e)} />
-            <PokemonBody info={pokemonData} />
+            <PokemonSearch value={userText} onChange={(e)=>setUserText(e.target.value)} />
+            <PokemonBody info={finalData} />
         </>
     )
 }
