f1d6731a820c254347c46733e03f54955fbce528
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-27 01:23:37 +0100

    Pokedex made responsive

--- src/components/pokemonPage/pokemonHeader/pokemonAttributeFilter/PokemonAttributeFilter.js
+++ src/components/pokemonPage/pokemonHeader/pokemonAttributeFilter/PokemonAttributeFilter.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './AttributeFilterStyling.css';
 import {buttonClassName} from '../../pokemonBody/pokemonAttribute/PokemonAttribute';
 
 const PokemonAttributeFilter = ({allAttributeFilters, onClickHandler}) => {
     return (
-        <div > 
+        <>
                 {allAttributeFilters.map((attributeTerm) => {
                     console.log('attributeTerm', attributeTerm)
                     const colorClass = buttonClassName(attributeTerm)
                     return <button className={`attribute-button ${colorClass}`} onClick={()=>onClickHandler(attributeTerm)}>{attributeTerm}</button>
                 })}
-        </div>
+        </>
     )
 }
 
